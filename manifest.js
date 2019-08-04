fb797fe7f11181821c36a0e3b0b1d5d0f2e0f7b2
aussiemaniacs <46294462+aussiemaniacs@users.noreply.github.com>
2019-08-04 22:24:41 +0930

    Add files via upload

---
+++ manifest.js
@@ -0,0 +1,13 @@
+mkdir my-stremio-addon
+cd my-stremio-addon
+cat > manifest.json
+{
+    "id": "4.20.smokin.stremio.add-on",
+    "version": "1.0.0",
+    "name": "4.20, Addon",
+    "description" : "4.20 smokin Stremio add-on ned kelly and proto",
+    "logo": "https://github.com/aussiemaniacs/420smokinaddon.io/blob/master/logo.png?raw=true",
+    "resources": [],
+    "types": []
+}
+^D
